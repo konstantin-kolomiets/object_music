0804601f0d2702033949cdc32fb88bd00534a0c4
vagrant <vagrant@devbox.vaimo.test>
2019-11-12 16:21:52 +0100

    front button edit

--- app/code/Vaimo/Events/view/frontend/web/js/view/element/buttonSubmit.js
+++ app/code/Vaimo/Events/view/frontend/web/js/view/element/buttonSubmit.js
@@ -3,12 +3,8 @@ define([
     ], function (Element) {
     'use strict';
     return Element.extend({
-        defaults: {
-            elementTmpl: 'view/element/buttonSubmit'
-        },
         action: function () {
-            // this.actions.forEach(this.applyAction, this);
-            console.log('asdasd');
+
         },
     })
 })
